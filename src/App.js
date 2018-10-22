4152508a28b234fa947e9a4502008c21295a8055
ACPerry <pansj@acloudchina.com>
2018-10-22 22:59:18 +0800

    single page navi 初版

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactCanvasNest from 'react-canvas-nest';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      title: 'Single Page Navi'
+    };
+  }
+
+  componentDidMount() {
+    fetch('./config.json').then((res) => {
+      return res.json();
+    }).then((res) => {
+      console.log(res);
+      this.setState({
+        items: res,
+        isLoaded: true
+      });
+    }).catch(error => console.error('Error: ', error));
+  }
+
   render() {
+    const { items,isLoaded, title } = this.state;
+    const projectList = items.map((value, index) => {
+      return <li key={value.name}><a className="list-link" href={value.link}>{value.name}</a></li>
+    });
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <img src={logo} className="ac-logo" alt="logo" />
+          <p className="ac-title">
+            {title}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          {
+            isLoaded && <ul className='list-container'>
+              {projectList}
+            </ul>
+          }
+
+          <ReactCanvasNest config={{follow: true, pointColor: '255,255,255', lineColor: '255,255,255'}} style={{zIndex: -1}} />
         </header>
       </div>
     );
